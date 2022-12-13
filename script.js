17cc425bbcfddc62960ff4179830bc304d4f2af6
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-12-13 13:33:20 -0500

    small changes

--- script.js
+++ script.js
@@ -44,33 +44,42 @@ var game;
 
 var queryURL = "https://api.rawg.io/api/games?key=48385021c28044a383e2de4c194654e4&dates=2019-09-01,2019-09-30&platforms=18,1,7" + game;
 
-function getApi(queryURL) {
-    fetch(queryURL)
+
+
+var getMovie = function (title) {
+    var apiUrl2 = '' + title + '&apikey=48385021c28044a383e2de4c194654e4';
+  
+    fetch(apiUrl2)
       .then(function (response) {
-        console.log(response);
-        if (response.status === 200) {
-          responseText.textContent = response.status;
+        if (response.ok) {
+          response.json().then(function (data) {
+              //console.log(data);
+            displayTitle(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
         }
-        return response.json();
-    });
+      })
+      .catch(function (error) {
+        alert('Unable to connect to RAWG Server');
+      });
+  };
+
+  var displayGame = function(data) {
+    var gameTitle = document.querySelector('.movieResults');
+    movieTitle.textContent = data.Title
+    var gamePlatforms= document.querySelector('.actors');
+    var movGenre = document.querySelector('.genre');
+    var moviePlot = document.querySelector('.plot');
+    var movieScore = document.querySelector('.metascore');
   }
   
-  getApi(queryURL);
-
-  var getMovie = function (movie) {
-      var apiUrl = 'http://www.omdbapi.com/?t=' + title + '&apikey=8da09333';
-    
-      fetch(apiUrl)
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              displayRepos(data, user);
-            });
-          } else {
-            alert('Error: ' + response.statusText);
-          }
-        })
-        .catch(function (error) {
-          alert('Unable to connect to GitHub');
-        });
-    };
+  
+  
+  var search = document.querySelector("#searchBar"); 
+  search.addEventListener("submit", function (event){
+      event.preventDefault();
+      var input = document.querySelector('#query');
+      var userInput = input.value
+      getMovie(userInput);
+})
