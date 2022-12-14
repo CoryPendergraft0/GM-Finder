499b8e019291a60cc8bacea006b9befe6c4f759f
unknown <tannerwlumsden@gmail.com>
2022-12-14 11:23:09 -0500

    finished game API and function

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+//requests and stores data from the video game API
+var getGame = function (game) {
+  var apiGame = 'https://api.rawg.io/api/games?key=48385021c28044a383e2de4c194654e4&search=' + game;
+  console.log(apiGame)
+
+  fetch(apiGame)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+          displayGame(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to RAWG Server');
+    });
+};
+
+//displays selected stored data to the webpage
+var displayGame = function(data) {
+  var gameTitle = document.querySelector('.gameResults');
+  gameTitle.textContent = data.results[0].name;
+  
+  var gameEsrb = document.querySelector('.esrb');
+  gameEsrb.textContent = data.results[0].esrb_rating.name;
+  
+  var gameRelease = document.querySelector('.release');
+  gameRelease.textContent = data.results[0].released;
+  
+  var gameGenre = document.querySelector('.gameGenre');
+  gameGenre.textContent = data.results[0].genres[0].name;
+  
+  var gamePlatform = document.querySelector('.platform');
+  gamePlatform.textContent = data.results[0].platforms[0].platform.name;
+}
+
+
 // requests and stores data from the movie API 
 var getMovie = function (title) {
   var apiUrl = 'http://www.omdbapi.com/?t=' + title + '&apikey=8da09333';
@@ -6,7 +46,7 @@ var getMovie = function (title) {
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-            //console.log(data);
+            console.log(data);
           displayTitle(data);
         });
       } else {
@@ -37,60 +77,11 @@ var displayTitle = function(data) {
 };
 
 
-
-
-
-var getGame = function (game) {
-    var apiGame = 'https://api.rawg.io/api/games/?search=' + game + 'key=48385021c28044a383e2de4c194654e4';
-    console.log(apiGame)
-  
-    fetch(apiGame)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-              console.log(data);
-            displayTitle(data);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to RAWG Server');
-      });
-  };
-  
-  var displayGame = function(data) {
-    var gameTitle = document.querySelector('.gameResults');
-    gameTitle.textContent = data.results.results[0].name;
-    
-    var gameEsrb= document.querySelector('.esrb');
-    gameEsrb.textContent = data.results.esrb_rating;
-    
-    var gameRelease = document.querySelector('.release');
-    gameRelease.textContent = data.results.released;
-    
-    var gameRating = document.querySelector('.rating');
-    gameRating.textContent = data.results.rating;
-    
-    var gameMeta = document.querySelector('.meta');
-    gameMeta.textContent = data.results.metacritic;
-  }
-  
-  
-  
-  var search = document.querySelector("#searchBar"); 
+var search = document.querySelector("#searchBar"); 
   search.addEventListener("submit", function (event){
       event.preventDefault();
       var input = document.querySelector('#query');
       var userInput = input.value
+      getGame(userInput);
       getMovie(userInput);
 });
-
-  var search = document.querySelector("#searchBar"); 
-  search.addEventListener("submit", function (event){
-    event.preventDefault();
-    var input = document.querySelector('#query');
-    var userInput = input.value
-    getGame(userInput);
-  })
